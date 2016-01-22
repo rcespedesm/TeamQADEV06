bfc0f749d5f7474267db1e602f4c364d5248b290
Rodrigo Saul Cespedes Murillo <rsc.murillo@gmail.com>
2016-01-22 11:06:58 -0400

    Class Calculator with 5 functions.

--- ClassExercises/1-19.js
+++ ClassExercises/1-19.js
@@ -2,45 +2,80 @@
  * Created by Administrator on 1/19/2016.
  */
 
+/**
+ * This class contain many functions to do math operations.
+ * @constructor
+ */
+var Calculator = function(){
 
-var doOperations = function(){
-    console.log('SUM is: ', getSum(arguments.length - 1, arguments));
-    console.log('MAX is: ', getMax(arguments.length, arguments, 0));
-    console.log('MIN is: ', getMin(arguments.length, arguments, 999999));
-};
+    /**
+     * doOperations is a function that call 4 math functions.
+     */
+    this.doOperations = function(){
+        console.log('SUM is: ', this.getSum(arguments.length - 1, arguments));
+        console.log('MAX is: ', this.getMax(arguments.length, arguments, 0));
+        console.log('MIN is: ', this.getMin(arguments.length, arguments, 999999));
+        console.log('AVG is: ', this.getAvg(arguments.length, arguments, 0));
+    };
 
-var getSum = function(index, list){
+    /**
+     * Get the sum of a number list
+     * @param index
+     * @param list
+     * @returns {*}
+     */
+    this.getSum = function(index, list){
 
-    if(index >= 0){
-        return list[index] + getSum(index - 1, list);
-    }
-    else{
-        return 0;
-    }
-};
+        if(index >= 0){
+            return list[index] + this.getSum(index - 1, list);
+        }
+        else{
+            return 0;
+        }
+    };
 
-var getMax = function(index, list, max){
-    if(index > 0){
-        if(max > list[index - 1])
-            return getMax(index - 1, list, max);
+    /**
+     * Get value maximum of a number list
+     * @param index this represent index to do a cycle.
+     * @param list this is the list with all numbers.
+     * @param max this store the maximum value to each cycle.
+     * @returns {*}
+     */
+    this.getMax = function(index, list, max){
+        if(index > 0){
+            if(max > list[index - 1])
+                return this.getMax(index - 1, list, max);
+            else
+                return this.getMax(index - 1, list, list[index - 1]);
+        }
+        else{
+            return max;
+        }
+    };
+
+    this.getMin = function(index, list, min){
+        if(index > 0){
+            if(min < list[index - 1])
+                return this.getMin(index - 1, list, min);
+            else
+                return this.getMin(index - 1, list, list[index - 1]);
+        }
+        else{
+            return min;
+        }
+    };
+
+    this.getAvg = function(qty, arg, index)
+    {
+        if (index == qty - 1) {
+            return arg[index];
+        }
+
+        if (index == 0)
+            return ((arg[index] + this.getAvg(qty, arg, index + 1)) / qty);
         else
-            return getMax(index - 1, list, list[index - 1]);
-    }
-    else{
-        return max;
-    }
+            return (arg[index] + this.getAvg(qty, arg, index + 1));
+    };
+
+
 };
-
-var getMin = function(index, list, min){
-    if(index > 0){
-        if(min < list[index - 1])
-            return getMin(index - 1, list, min);
-        else
-            return getMin(index - 1, list, list[index - 1]);
-    }
-    else{
-        return min;
-    }
-};
-
-
